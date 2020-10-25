08b8d39b619b30201c559b0e588223fcaf004427
Sudarshan Dilip Gundale <53350481+5udarshan@users.noreply.github.com>
2020-10-25 17:52:37 +0530

    Add files via upload

---
+++ practical 1a.py
@@ -0,0 +1,42 @@
+#Write a program to store the elements in 1-D array and provide an option
+#to perform the operations like searching, sorting, merging, reversing
+#the elements
+# 3043 (New Roll No:392)
+
+class ArrayModification:
+            
+    def linear_search(self,lst,n):
+        for i in range(len(lst)):
+            if lst[i] == n:
+                return f'Position :{i}'
+        return -1
+    
+    def insertion_sort(self,lst):
+        for i in range(len(lst)):
+            
+            index = lst[i]
+            
+            k = i - 1
+            
+            while k >= 0 and lst[k] > index:
+                lst[k + 1] = lst[k]
+                k -= 1
+                
+            lst[k+1]  = index
+            
+        return lst
+    
+    def merge(self,lst,lst2):
+         lst.extend(lst2)
+         print(lst)
+    
+    def reverse(self,lst):
+        return lst[::-1]
+
+lst = [2,9,1,7,3,5,2]
+lst2 = [4,6,8,9,4,5]
+Arrmod = ArrayModification()
+print(Arrmod.linear_search(lst,3))
+print(Arrmod.merge(lst,lst2))
+print(Arrmod.insertion_sort(lst))
+print(Arrmod.reverse(lst))
